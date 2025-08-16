f45f8f2970b4da8ed52e6b77db87a23cd2948bd3
Luka2010lab <116589575+Luka2010lab@users.noreply.github.com>
2025-08-16 20:25:14 +0200

    Delete luka directory

--- luka/gra1/NakrycieGlowy.py
+++
@@ -1,5 +0,0 @@
-from Element import Element
-
-class Nakrycieglowy(Element):
-    def __init__(self,):
-        super().__init__('head')
--- luka/snake/Dieuction.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-
-class Dirction(Enum):
-    Gora = 0
-    Prawo = 1
-    Dol = 2
-    Lewo = 3
