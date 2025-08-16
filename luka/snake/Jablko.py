8c0d22414ea041d491dd315fdbd72dc18abed995
Luka2010lab <116589575+Luka2010lab@users.noreply.github.com>
2025-08-16 20:24:57 +0200

    Delete luka directory

--- luka/snake/Jablko.py
+++
@@ -1,9 +0,0 @@
-import pygame
-import random
-
-class Jablko(pygame.sprite.Sprite):
-    def __init__(self):
-        super(Jablko, self).__init__()
-        self.obraz = pygame.image.load("images/apple.png")
-        losowa_pozycja = pygame.Rect(random.randrange(1,25)*32, random.randrange(1,19)*32, 32, 32)
-        self.pozycja = losowa_pozycja
